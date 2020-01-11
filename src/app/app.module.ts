29ee0a783e32be25f749cd14e35c950b81f104c6
sarongTech <ikhawahyuramadhan@gmail.com>
2020-01-11 16:21:32 +0700

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
